eb9a0aba3bd675789a5db3bf8b649fe72e15a7df
Hassan Lakhal <hlakhal.contact@gmail.com>
2026-04-22 23:30:41 +0100

    add function of forward

--- src/layers.py
+++ src/layers.py
@@ -5,6 +5,9 @@ class DenseLayer :
         self.initializer = weights_initializer
         self.weights = None
         self.bias = None
+
+        self.input = None
+        self.z = None
     
     def initialize(self, input_size):
         if  self.initializer == 'heUniform':
@@ -13,4 +16,12 @@ class DenseLayer :
         else:
             self.weights = np.random.randn(input_size, self.units) * 0.01
         
-        self.bias = np.zeros((1, self.units))  
+        self.bias = np.zeros((1, self.units)) 
+
+    def forward(self, input_data):
+        self.input = input_data
+
+        self.z = np.dot(self.input, self.weights) + self.bias
+
+        return self.z
+
