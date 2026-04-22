f97e00f817243c31a0027948d66079f66b333bee
Hassan Lakhal <hlakhal.contact@gmail.com>
2026-04-22 22:40:24 +0100

    add class of create layers

---
+++ src/layers.py
@@ -0,0 +1,16 @@
+class DenseLayer :
+    def __init__(self, units, activation='sigmoid', weights_initializer='default'):
+        self.units = units
+        self.activation = activation
+        self.initializer = weights_initializer
+        self.weights = None
+        self.bias = None
+    
+    def initialize(self, input_size):
+        if  self.initializer == 'heUniform':
+            limit = np.sqrt(6/ input_size)
+            self.weights = np.random.uniform(-limit, limit, (input_size, self.units))
+        else:
+            self.weights = np.random.randn(input_size, self.units) * 0.01
+        
+        self.bias = np.zeros((1, self.units))  
