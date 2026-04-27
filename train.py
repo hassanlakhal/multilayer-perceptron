3d230d7bc02239b13e649116cca261fa56a992c9
Hassan Lakhal <hlakhal.contact@gmail.com>
2026-04-27 10:45:37 +0100

    add Multilayer Perceptron Training

--- train.py
+++ train.py
@@ -5,29 +5,44 @@ from split_data import load_and_split_data
 from src.utils import save_model, save_history
 model = Model()
 
-input_shape = 30
-output_shape = 2
-optimizer = 'rmsprop'
-network = model.createNetwork([
-layers.DenseLayer(input_shape, activation='sigmoid',optimizer=optimizer),
-layers.DenseLayer(24, activation='sigmoid', weights_initializer='heUniform',optimizer=optimizer),
-layers.DenseLayer(24, activation='sigmoid', weights_initializer='heUniform',optimizer=optimizer),
-layers.DenseLayer(24, activation='sigmoid', weights_initializer='heUniform',optimizer=optimizer),
-layers.DenseLayer(output_shape, activation='softmax', weights_initializer='heUniform',optimizer=optimizer)
-])
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Multilayer Perceptron Training")
+    
+    parser.add_argument('--layers', nargs='+', type=int, default=[24, 24, 24], help='List of hidden layer units')
+    parser.add_argument('--epochs', type=int, default=84, help='Number of epochs')
+    parser.add_argument('--loss', type=str, default='categoricalCrossentropy', help='Loss function')
+    parser.add_argument('--batch_size', type=int, default=8, help='Batch size')
+    parser.add_argument('--learning_rate', type=float, default=0.0314, help='Learning rate')
+    parser.add_argument('--optimizer', type=str, default='sgd', help='Optimizer Algo (sgd or rmsprop)')
+    return parser.parse_args()
 
 
-X_train, X_test, y_train, y_test = load_and_split_data('data/data.csv')
+if __name__ ==  '__main__':
+
+    args = get_args()
+    print(f"Training with {args.optimizer}...")
+    opt = args.optimizer
+    input_layer = [layers.DenseLayer(30, activation='sigmoid', weights_initializer='heUniform', optimizer=opt)]
+    hidden_layers = []
+    for units in args.layers :
+        hidden_layers.append(layers.DenseLayer(units, activation='sigmoid', weights_initializer='heUniform', optimizer=opt))
+        print(units)
+    output_layer = [layers.DenseLayer(2, activation='sigmoid', weights_initializer='heUniform', optimizer=opt)]
+
+    all_layers = input_layer + hidden_layers + output_layer
+    network = model.createNetwork(all_layers)
+
+    X_train, X_test, y_train, y_test = load_and_split_data('data/data.csv')
 
 
-history = model.fit(network, (X_train, y_train), (X_test, y_test), 
+    history = model.fit(network, (X_train, y_train), (X_test, y_test), 
                     loss='categoricalCrossentropy', 
                     learning_rate=0.001, 
                     batch_size=8, 
                     epochs=70)
+                    
 
-save_model(network)
-
-
-history_filename = f"model/history_{optimizer}.json"
-save_history(history, history_filename)
+    history_filename = f"model/history_{opt}.json"
+    save_history(history, history_filename)
